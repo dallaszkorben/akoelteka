99ad27dc7d982d06d3c44011d4965b1f4c373d48
dallaszkorben <dallaszkorben@gmail.com>
2018-12-03 23:15:42 +0100

    This version is to pip

--- akoteka/handle_property.py
+++ akoteka/handle_property.py
@@ -1,8 +1,6 @@
 import os
 import configparser
 
-PROJECT = 'akoteka'
-
 class Property(object):
     """
     This singleton handles the package's ini file.
@@ -98,8 +96,7 @@ class Dict( Property ):
         return inst
         
     def __init__(self, lng):
-        #file = os.path.join(os.getcwd(), self.__class__.DICT_FOLDER, self.__class__.DICT_FILE_PRE + "_" + lng + "." + self.__class__.DICT_FILE_EXT)
-        file = os.path.join(os.getcwd(), PROJECT, self.__class__.DICT_FOLDER, self.__class__.DICT_FILE_PRE + "_" + lng + "." + self.__class__.DICT_FILE_EXT)
+        file = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.__class__.DICT_FOLDER, self.__class__.DICT_FILE_PRE + "_" + lng + "." + self.__class__.DICT_FILE_EXT)
         super().__init__( file )
     
     def _(self, key):
@@ -129,7 +126,7 @@ class ConfigIni( Property ):
         return inst
         
     def __init__(self):
-        file = os.path.join(os.getcwd(), PROJECT, ConfigIni.INI_FILE_NAME)
+        file = os.path.join(os.path.dirname(os.path.abspath(__file__)), ConfigIni.INI_FILE_NAME)
         super().__init__( file )
 
 
