e9618eeda89beb12315032d2af91900d9c6ffaaa
dallaszkorben <dallaszkorben@gmail.com>
2019-04-14 18:13:49 +0200

    #79 - Fix: tool_ini_check

--- card_ini_correction.py
+++
@@ -1,4 +0,0 @@
-from akoteka.tools import change_ini
-
-if __name__ == "__main__":
-    change_ini.main()
--- healthcheck.py
+++
@@ -1,4 +0,0 @@
-from akoteka.tools import health_check
-
-if __name__ == "__main__":
-    health_check.main()
